ab5a201af49169ca152890b6df22c850797151fe
Ashutosh Singh <ashuforweb@gmail.com>
2016-01-18 12:25:02 -0800

    Init
    Init

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Ashutosh singh on 1/18/16.
+//  Copyright © 2016 ashuvashu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
